1bc20b9a989357fd78461e38ae8cb330b8aae7d5
thonh <thonh@users.noreply.github.com>
2024-11-01 17:04:19 +0700

    Update Swipeable.js

--- components/Cards/Swipeable.js
+++ components/Cards/Swipeable.js
@@ -1,11 +1,25 @@
-import { View, Text } from "react-native";
-import React from "react";
+// components/Cards/Swipeable.js
+import React from 'react';
+import { FlatList, SafeAreaView } from 'react-native';
+import SwipeableCard from './SwipeableCard';
+
+const data = [
+  { id: '1', title: 'Card 1', description: 'Description for card 1' },
+  { id: '2', title: 'Card 2', description: 'Description for card 2' },
+  // Thêm nhiều card tùy ý
+];
 
 const Swipeable = () => {
   return (
-    <View>
-      <Text>Swipeable</Text>
-    </View>
+    <SafeAreaView>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <SwipeableCard title={item.title} description={item.description} />
+        )}
+      />
+    </SafeAreaView>
   );
 };
 
