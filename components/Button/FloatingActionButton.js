fa650c9d7d1c99ec505203e8ae9df914d74c7547
Thang Le <trkien22@gmail.com>
2024-11-01 15:37:54 +0700

    update

--- components/Button/FloatingActionButton.js
+++ components/Button/FloatingActionButton.js
@@ -1,12 +1,50 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { View, StyleSheet } from "react-native";
+import { FloatingAction } from "react-native-floating-action";
+import Icon from "react-native-vector-icons/Ionicons";
 
-const FloatingActionButton = () => {
+const FloatingActionExample = () => {
+  const actions = [
+    {
+      text: "Add",
+      name: "add",
+      color: "#3498db",
+      icon: <Icon name="add-outline" size={24} color="#fff" />,
+    },
+    {
+      text: "Search",
+      name: "search",
+      color: "#1abc9c",
+      icon: <Icon name="search-outline" size={24} color="#fff" />,
+    },
+    {
+      text: "Edit",
+      name: "edit",
+      color: "#e74c3c",
+      icon: <Icon name="pencil-outline" size={24} color="#fff" />,
+    },
+  ];
+
   return (
-    <View>
-      <Text>FloatingActionButton</Text>
+    <View style={styles.container}>
+      <FloatingAction
+        actions={actions}
+        color="#3498db"
+        onPressItem={(name) => {
+          alert(`Selected action: ${name}`);
+        }}
+      />
     </View>
   );
 };
 
-export default FloatingActionButton;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f8f8f8",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default FloatingActionExample;
